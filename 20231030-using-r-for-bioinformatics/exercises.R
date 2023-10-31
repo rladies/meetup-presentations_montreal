82d11ec5e9103eec759be0b802e3f52833609d11
Rhiannon <rhiannon@precision-analytics.ca>
2023-10-30 21:42:03 -0400

    Add exercise solutions for MTL October 2023 meetup

--- 20231030-using-r-for-bioinformatics/exercises.R
+++ 20231030-using-r-for-bioinformatics/exercises.R
@@ -4,15 +4,33 @@
 # ?length
 
 # 1. Install the Bioconductor package Biostrings .
-
+install.packages("BiocManager")
+BiocManager::install("Biostrings")
 
 # 2. Store at least 2 DNA sequences in a DNAStringSet object.
+library(Biostrings)
+# GCTA
+dna <- c("CTGTGC", "TGA", "CCT")
+dna_new <- DNAStringSet(dna)
 
+dna1 <- DNAString("TG")
+dna2 <- DNAString("GCGC")
+dna_new2 <- DNAStringSet(list(dna1, dna2))
 
 # 3. Translate your DNA into amino acids.
 # Bonus 1: Try incorporating ambiguous bases in your DNA. Test different if.fuzzy.codon options of translate().
+translate(DNAStringSet(dna))
 
+aas <- translate(dna_new)
+translate(DNAString("TGAHGA"), if.fuzzy.codon = "solve")
 
 # 4. Count the number of methionine (M) residues in each sequence.
 # (If you have no methionine, try a different amino acid!)
 # Bonus 2: Summarize the distribution across all of your sequences.
+counts <- alphabetFrequency(aas)
+
+df <- data.frame(counts)
+df$M
+summary(df$M)
+
+
